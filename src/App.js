66ee17337780ba86dd754a2246b3a77984ef3ffc
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-12-03 12:31:37 -0800

    Final actvitity

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@
 
 // Import components
 import './App.css';
+import Directory from './Components/Directory'
+import Directory from './Components/Gallery'
+import Directory from './Components/Searchbar'
+import Directory from './Components/Sidebar'
 
-function App(){
+const App = () => {
   return (
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+       <SearchBar />
+       <Directory />
+       <Sidebar />
+       <Gallery postings={postings} />
       </div>
     </div>
   );
